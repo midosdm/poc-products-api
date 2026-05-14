13df6f04867ab78ae70df623ac5b39e9d45d5a26
mohamed.saidem <mohamed.saidem@accenture.com>
2026-05-14 14:17:50 +0200

    Initial commit

---
+++ src/main/java/com/api/pocproductsapi/PocProductsApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.pocproductsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocProductsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocProductsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/pocproductsapi/PocProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.pocproductsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocProductsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
